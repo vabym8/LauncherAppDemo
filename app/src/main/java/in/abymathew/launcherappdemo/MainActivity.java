cc24f7b824900bccae9c4c59ed164742d9bb8ec6
Aby Mathew <vabym8@gmail.com>
2017-02-24 13:32:50 +0300

    Initial commit

---
+++ app/src/main/java/in/abymathew/launcherappdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package in.abymathew.launcherappdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
